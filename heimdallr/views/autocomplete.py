1df42f149208105456822a38526c747829ccf5c1
Adam Findlay <skylinerspeeder@gmail.com>
2017-01-04 05:45:58 +0000

    Added alliance, corp and character autocomplete endpoints

--- heimdallr/views/autocomplete.py
+++ heimdallr/views/autocomplete.py
@@ -5,16 +5,67 @@ from bson.json_util import dumps
 from heimdallr import app
 from heimdallr.db import db
 
-
 # Returns autocomplete results for alliance search
 @app.route("/autocomplete/alliance/<string:search>", methods=['GET'])
 def alliance(search):
     # Build Mongo search object
     searchobj = {
-                "killmail.victim.alliance.name": {
-                    "$regex": "%s.*" % search
+        "$or": [
+            {
+                "name": {
+                    "$regex": "^%s" % search,
+                    '$options' : 'i',
                 }
+            }, {
+                "ticker": {
+                    "$regex": "^%s" % search,
+                    '$options' : 'i',
+                }
+            }
+        ]
     }
 
-    r = db.kills.find(searchobj)
+    projection = {"_id": False }
+    r = db.alliances.find(searchobj, projection=projection, limit=50)
+    return Response(response=dumps(r), status=200, mimetype="application/json")
+
+
+# Returns autocomplete results for corporation search
+@app.route("/autocomplete/corporation/<string:search>", methods=['GET'])
+def corporation(search):
+    # Build Mongo search object
+    searchobj = {
+        "$or": [
+            {
+                "name": {
+                    "$regex": "^%s" % search,
+                    '$options' : 'i',
+                }
+            }, {
+                "ticker": {
+                    "$regex": "^%s" % search,
+                    '$options' : 'i',
+                }
+            }
+        ]
+    }
+
+    projection = {"_id": False }
+    r = db.corporations.find(searchobj, projection=projection, limit=50)
+    return Response(response=dumps(r), status=200, mimetype="application/json")
+
+
+# Returns autocomplete results for character search
+@app.route("/autocomplete/character/<string:search>", methods=['GET'])
+def character(search):
+    # Build Mongo search object
+    searchobj = {
+        "name": {
+            "$regex": "^%s" % search,
+            '$options' : 'i',
+        }
+    }
+
+    projection = {"_id": False }
+    r = db.characters.find(searchobj, projection=projection, limit=50)
     return Response(response=dumps(r), status=200, mimetype="application/json")
